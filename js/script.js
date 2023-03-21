e81a2bea1f0bb2d7386148adeb8d00650f4865f8
Dan848 <daniele.ceravolo15@gmail.com>
2023-03-21 18:07:28 +0100

    fixed timer

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 /*
 Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi.
 Dopo 30 secondi i numeri scompaiono e l'utente deve inserire, i numeri che ha visto precedentemente.
-Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da ricordare sono stati ricordati.
 */
 
 function getRndInteger(min, max) {
@@ -60,7 +60,7 @@ function play () {
     printNumbers.innerHTML = "";
     start.addEventListener("click", play);
     check.addEventListener("click", result);
-    }, 3000);
+    }, 30000);
 
 
 
